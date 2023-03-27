32b8aa1a06f2d8472989cd556c188b2c0ce54fc8
Annika Schreiner <45626818+anniSc@users.noreply.github.com>
2023-03-27 10:37:18 +0200

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -7,7 +7,7 @@ st.title('ML4B 2023')
 
 #checkbox
 if st.checkbox('Show Image'):
-    image = Image.open('./images/puppy.jpeg')
+    image = Image.open('./puppy.jpeg')
     st.image(image, caption='Running Puppy')
 
 st.write("Here's my data:")
